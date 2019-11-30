1efa3ba9d3c21a713f17c0941313fdb041fe56fd
Bradley Haley <bshaley25@gmail.com>
2019-11-30 18:20:56 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,11 @@
 def square_array(array)
   
+  b = []
   counter = 0
  
   while array[counter] do
-    array[counter]**
+    b.push(array[counter]**2 )
     counter += 1
   end
-  
+  p b 
 end
-
